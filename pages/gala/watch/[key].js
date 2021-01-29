5ec99ec4fa3d1c5bc9c1f9b9dd4093ef37ee9fc9
Craig Baker <cbaker@jpgacademy.org>
2021-01-28 20:46:19 -0600

    Rember youtube-player

--- pages/gala/watch/[key].js
+++ pages/gala/watch/[key].js
@@ -2,7 +2,6 @@ import React from "react"
 import Head from "next/head"
 import Link from "next/link"
 import { useRouter } from "next/router"
-import YouTube from "react-youtube"
 import ReactPlayer from "react-player/youtube"
 
 import { DB } from "../../../lib"
@@ -32,33 +31,7 @@ function Watch(props) {
           style={{ backgroundImage: "url(/images/jpg.jpg)" }}
         ></div>
       </section>
-      {/* <YouTube
-        videoId={id}
-        onReady={_onReady}
-        opts={{
-          playerVars: {
-            disablekb: 1,
-            enablejsapi: 1,
-            modestbranding: 1,
-            playsinline: 1,
-            loop: 1,
-            autoplay: 1,
-            controls: 0,
-            showinfo: 0,
-            cc_load_policy: 0,
-            iv_load_policy: 3,
-            autohide: 1,
-          },
-        }}
-      /> */}
-      {/* <iframe
-        width="560"
-        height="315"
-        src='https://www.youtube.com/embed/Yb7jFafo3x4?autoplay=1&embed_config={"primaryThemeColor":"#FF0000","relatedChannels":["UCCpY-hecoW6hn3Gx7wpVyRw","UCWJ2lWNubArHWmf3FIHbfcQ"],"relatedVideos":["FXLk2rCG1yg","Yb7jFafo3x4"],"autonav":true,"disableRelatedVideos":true}'
-        frameBorder="0"
-        allow="autoplay"
-        allowFullScreen
-      ></iframe> */}
+
       <PlayerMine videoId="ADY8cqXYYIw" />
 
       <Footer />
